00f1b67b578f178054db714d19ef352e159c10c6
Pabl-OS <pablito99sant@gmail.com>
2022-08-10 17:02:40 -0500

    feat :   character layout and character detail

--- src/app/character/character-routing.module.ts
+++ src/app/character/character-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
         component:CharacterListComponent
       },
       {
-        path:'detalle',
+        path:'detalle/:id',
         component:CharacterDetailComponent
       },
       {
--- src/app/character/components/card-character/card-character.component.ts
+++ src/app/character/components/card-character/card-character.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from 'src/app/interfaces/Characters';
 
 @Component({
   selector: 'app-card-character',
   templateUrl: './card-character.component.html'
 })
 export class CardCharacterComponent implements OnInit {
-
+  public character!:Character;
   constructor() { }
 
   ngOnInit(): void {
